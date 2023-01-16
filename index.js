39fb78c5a194102ce645775633c510cbd0a9be80
Samuel <samuelou510@gmail.com>
2023-01-16 02:59:59 -0500

    commit tree 58ffe7edb272d491c47e6bcf1d878ec0fb942973

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {}
+
+employee["name"] = "Sam";
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let copyEmp = { ...employee };
+    copyEmp[key] = value;
+    return copyEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let copyEmp = { ...employee };
+    delete copyEmp[key];
+    return copyEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+

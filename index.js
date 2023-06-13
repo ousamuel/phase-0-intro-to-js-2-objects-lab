30487bf8167d2158258917d8f9fe601bae873486
Samuel <samuelou510@gmail.com>
2023-06-13 16:27:57 -0400

    objects redone

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+let employee = {
+  name: "Sam",
+  streetAddress : "5",
+}
+function updateEmployeeWithKeyAndValue(emp, key, value) {
+  const newEmp = {...emp};
+  newEmp[key] = value;
+  return newEmp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(emp, key, value) {
+  employee = emp;
+  employee[key] = value;
+  return employee;
+}
+
+function deleteFromEmployeeByKey(emp, key) {
+  const newEmp = {...emp}
+  delete newEmp[key];
+  return newEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(emp, key) {
+  delete emp[key];
+  return emp;
+}
